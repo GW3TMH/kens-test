94e93d2aa09becf1d320c31f0412688e0c08d856
Ken Williams <33055092+GW3TMH@users.noreply.github.com>
2023-03-14 11:27:27 +0000

    Add files via upload

--- setup.py
+++ setup.py
@@ -3,25 +3,18 @@
 
 from setuptools import setup
 
-authors_name = 'Ken Williams'
-authors_email = 'ken@kensmail.uk'
-github_url = 'https://github.com/GW3TMH/kens-test'
+
 package_name = 'kens-test'
-short_description = 'Demonstrates TKinter snap'
+filename = 'kens-test/__init__.py'
+
 
 def get_version():
     import ast
-    
-    filename = package_name +'/__init__.py'
-    
-    try:
-        with open(filename) as input_file:
-            for line in input_file:
-                if line.startswith('__version__'):
-                    input_file.close()
-                    return ast.parse(line).body[0].value.s
-    except IOError:
-        return '1.0'
+
+    with open(filename) as input_file:
+        for line in input_file:
+            if line.startswith('__version__'):
+                return ast.parse(line).body[0].value.s
 
 
 def get_long_description():
@@ -29,28 +22,31 @@ def get_long_description():
         with open('README.md', 'r') as f:
             return f.read()
     except IOError:
-        return 'Long Description'
+        return ''
 
 
 def get_requirements():
-    try:
-        with open('requirements.txt', 'r') as f:
-            return f.read().splitlines()
-    except IOError:
-        return "['None']"
+    with open('requirements.txt', 'r') as f:
+        return f.read().splitlines()
 
 
 setup(
     name=package_name,
     version=get_version(),
     install_requires=get_requirements(),
-    author=authors_name,
-    author_email=authors_email,
-    description=short_description,
-    url=github_url,
+    author='Ken Williams',
+    author_email='ken@kensmail.uk',
+    description='Demo of using Tkinter',
+    url='https://github.com/GW3TMH/kens-test',
     long_description=get_long_description(),
     packages=[package_name, 'bin'],
     include_package_data=True,
-    entry_points = "{'console_scripts': ['" +package_name + " = bin." +package_name +":main']}",
+    entry_points={
+        'console_scripts': [
+            'kens-test = bin.kens-test:main'
+        ]
+    },
     license='License :: OSI Approved :: MIT License',
+    
 )
+
